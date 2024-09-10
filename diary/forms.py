99644cf24b2e88b68a2e1f1ec703873ac7ab8860
dashuta <it.dashutka@gmail.com>
2024-09-11 00:13:22 +0300

    3rd commit

--- diary/forms.py
+++ diary/forms.py
@@ -1,8 +1,10 @@
 from diary.models import Note
 from django import forms
+
 from users.forms import StyleFormMixin
+
 
 class NoteForm(StyleFormMixin, forms.ModelForm):
     class Meta:
         model = Note
-        fields = ('topic', 'body', 'owner', )
+        fields = '__all__'
--- users/models.py
+++ users/models.py
@@ -1,16 +1,25 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from diary.models import NULLABLE
 
 
 class User(AbstractUser):
+    FEMALE_GENDER = 'female'
+    MALE_GENDER = 'male'
+
+    GENDERS = (
+        (FEMALE_GENDER, 'женский'),
+        (MALE_GENDER, 'мужской')
+    )
+
     username = None
 
-    email = models.EmailField(unique=True, verbose_name='Email')
+    email = models.EmailField(unique=True, verbose_name='Email', help_text='Укажите email')
 
-    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    avatar = models.ImageField(upload_to='users/', verbose_name='аватар', help_text='Загрузите аватар', **NULLABLE)
+
+    """Указание гендера необходимо для выбора подходящей заглушки аватара."""
+    gender = models.CharField(choices=GENDERS, verbose_name='пол', help_text='Выберите пол', **NULLABLE)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
